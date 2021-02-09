dc3500135a562157e80f6e069c55a48c0fa1d93e
Husen Efendi <husenef@gmail.com>
2021-02-09 15:52:38 +0700

    update seeder

--- seeders/20200818040101-add-user-date.js
+++ seeders/20200818040101-add-user-date.js
@@ -1,38 +1,45 @@
-'use strict';
-
+"use strict";
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSaltSync(10);
+// const hashedPassword = bcrypt.hashSync(password, salt);
+const genPas = (text) => bcrypt.hashSync(text, salt);
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Users", [{
-      full_name: "John Doe",
-      username: "johndoe",
-      password: "passjohn",
-      salt: "",
-      email: "johndoe@gmail.com",
-      phone_number: "087879431",
-      role: "admin",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      full_name: "Sam Doe",
-      username: "samdoe",
-      password: "passsam",
-      salt: "",
-      email: "samdoe@gmail.com",
-      phone_number: "087879489",
-      role: "admin",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      full_name: "Jhonny Doe",
-      username: "jhonnydoe",
-      password: "passjohnny",
-      salt: "",
-      email: "jhonnydoe@gmail.com",
-      phone_number: "087879441",
-      role: "admin",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }])   
+    return queryInterface.bulkInsert("Users", [
+      {
+        full_name: "John Doe",
+        username: "johndoe",
+        password: genPas("passjhon"),
+        salt: salt,
+        email: "johndoe@gmail.com",
+        phone_number: "087879431",
+        role: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        full_name: "Sam Doe",
+        username: "samdoe",
+        password: genPas("passsam"),
+        salt: salt,
+        email: "samdoe@gmail.com",
+        phone_number: "087879489",
+        role: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        full_name: "Jhonny Doe",
+        username: "jhonnydoe",
+        password: genPas("passjhonny"),
+        salt: salt,
+        email: "jhonnydoe@gmail.com",
+        phone_number: "087879441",
+        role: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -42,6 +49,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    return queryInterface.bulkDelete("Users", null, {})
-  }
+    return queryInterface.bulkDelete("Users", null, {});
+  },
 };
